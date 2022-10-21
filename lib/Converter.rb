4f98c3332600c0c0a1792d7bfb2ee0994fc3ecb4
FViKki07 <73124000+FViKki07@users.noreply.github.com>
2022-10-21 11:47:43 +0300

    Squashed commit of the following:
    commit 08bdc7298c2e0ff78f1cee407f10284b713cc134
    Merge: 43ba8f5 96e7f4d
    Author: Alina <93431239+Alina0101@users.noreply.github.com>
    Date:   Thu Oct 20 15:21:12 2022 +0300
    
        Merge pull request #3 from AnastasiaDb/TableSpoon
    
        Table spoon
    
    commit 96e7f4d84b8d595cde3dd35901a25041030082fa
    Merge: d8622b2 43ba8f5
    Author: Alina <93431239+Alina0101@users.noreply.github.com>
    Date:   Thu Oct 20 15:20:47 2022 +0300
    
        Edit Coverter
    
        Merge two branch(gram and tablespoon)
    
    commit 43ba8f504c9dbf66e04a7ff53886c6464b4cdd56
    Merge: 281d9bf e5089d1
    Author: FViKki07 <73124000+FViKki07@users.noreply.github.com>
    Date:   Thu Oct 20 15:13:08 2022 +0300
    
        Merge pull request #2 from AnastasiaDb/Grams
    
        Grams
    
    commit d8622b2086aa2f5d85679372d4bfe913750d1e45
    Author: Alina <93431239+Alina0101@users.noreply.github.com>
    Date:   Thu Oct 20 15:07:30 2022 +0300
    
        Edit tablespoon
    
        Add functions to Converter for convert from tablespoon
    
    commit 81772fb733d9b1ee09aabd718378b318b810e50b
    Author: Alina <93431239+Alina0101@users.noreply.github.com>
    Date:   Thu Oct 20 14:42:12 2022 +0300
    
        Edit tableSpoon
    
    commit 3c5c7aa1978aa270846090d83049f77bd242ac12
    Author: Alina <93431239+Alina0101@users.noreply.github.com>
    Date:   Wed Oct 19 15:31:17 2022 +0300
    
        add convert to tablespoon

--- lib/Converter.rb
+++ lib/Converter.rb
@@ -1,12 +1,41 @@
 # frozen_string_literal: true
 
 require_relative "Converter/version"
+require_relative "tableSpoon"
 require_relative "gram"
 
 module Converter
-  class Error < StandardError; end 
+  class Error < StandardError; end
+  # Your code goes here...
+  extend self 
 
-  extend self
+  def tablesp2kilo(n, p = 1)
+    TableSpoon.to_kilo(n, p)
+  end
+
+  def tablesp2ml(n, p = 1)
+    TableSpoon.to_ml(n, p)
+  end
+
+  def tablesp2gramm(n, p = 1)
+    TableSpoon.to_gram(n, p)
+  end
+
+  def tablesp2liter(n, p = 1)
+    TableSpoon.to_liter(n, p)
+  end
+
+  def tablesp2deciliter(n, p = 1)
+    TableSpoon.to_deciliter(n, p)
+  end
+  
+  def tablesp2glass(n, p = 1)
+    TableSpoon.to_glass(n, p)
+  end
+
+  def tablesp2teaspoon(n, p = 1)
+    TableSpoon.to_teaspoon(n, p)
+  end
 
   def gram2kilo(m,p = 1)
     Gram.to_Kilo(m, p)
