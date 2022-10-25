3cb9c89e1e176f59e4e7d283a21e5c4733c41980
FViKki07 <73124000+FViKki07@users.noreply.github.com>
2022-10-25 20:05:32 +0300

    update kilo

--- lib/kilo.rb
+++ lib/kilo.rb
@@ -1,2 +1,37 @@
 class Kilo 
+    
+    def self.to_gram(kilo)
+        kilo < 0 ? (kilo * 1000.0 * -1) : (kilo * 1000.0) 
+    end
+
+    def self.to_ml(kilo, p)
+        kilo < 0 ? (kilo / p * -1000.0) : (kilo / p * 1000.0) 
+    end   
+
+    def self.to_liter(kilo, p)
+        (kilo < 0) ? (kilo / p * -1.0) : (kilo / p * 1.0)
+    end    
+
+    def self.to_diciliter(kilo, p)
+        (kilo < 0) ? (kilo / p * -10.0) : (kilo / p * 10.0)
+    end
+
+    def self.to_tablespoon(kilo, p)
+        (kilo<0) ? (gr / p *1000 / -18.0) : (gr / p * 1000 / 18.0) #??????? 
+    end    
+
+    def self.to_glass(gr, p)
+        if(gr<0)
+            gr / p /-200.0
+        else gr / p /200.0
+        end
+    end
+
+    def self.to_teaspoon(gr, p)
+        if(gr<0)
+            gr / p / -5.0
+        else gr / p / 5.0
+        end    
+    end    
+
 end    
