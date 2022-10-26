11dfa483bec1c0502052d4ee898e7efb64c3651d
AnastasiaDb <nastya_nastya_nastya.ru@mail.ru>
2022-10-26 16:45:48 +0300

    Create liter.rb

---
+++ lib/liter.rb
@@ -0,0 +1,59 @@
+class Liter
+
+  def self.to_kilo(l, p = 1)
+    if (l < 0)
+      l * p * -1
+    else
+      l * p
+    end
+  end
+
+  def self.to_ml(l)
+    if (l < 0)
+      l * -1000
+    else
+      l * 1000
+    end
+  end
+
+  def self.to_gram(l, p = 1)
+    if (l < 0)
+      l * p * -1000
+    else
+      l * p * 1000
+    end
+  end
+
+  def self.to_deciliter(l)
+    if (l < 0)
+      l * -10
+    else
+      l * 10
+    end
+  end
+
+  def self.to_glass(l)
+    if (l < 0)
+      l * 1000 / -200
+    else
+      l * 1000 / 200
+    end
+  end
+
+  def self.to_teaspoon(l)
+    if (l < 0)
+      l * 1000 / -5
+    else
+      l * 1000 / 5
+    end
+  end
+
+  def self.to_tablespoon(l)
+    if (l < 0)
+      l * 1000 / -18
+    else
+      l * 1000 / 18
+    end
+  end
+
+end
